4a14db1c401f95fe53011255ddedde4d8763d1ae
pwlimaverde <pwlimaverde@gmail.com>
2024-10-06 12:47:27 -0300

    firebase ok

--- adf_jornada_flutter/hands-on/todo_list_provider/lib/main.dart
+++ adf_jornada_flutter/hands-on/todo_list_provider/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
 import 'firebase_options.dart';
@@ -7,8 +9,15 @@ import 'src/app_widget.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-  options: DefaultFirebaseOptions.currentPlatform,
-);
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  final instance = FirebaseFirestore.instance;
+
+  final collection = await instance.collection("teste").doc("testedoc").get();
+  final teste = collection.data();
+
+  print(teste);
+
   runApp(const AppWidget());
 }
-
